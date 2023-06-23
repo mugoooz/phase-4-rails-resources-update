a300d0db3b65c651d53dce1ec13ac38f4b6e1510
Eric Mugo <mugoericpg20@gmail.com>
2023-06-23 16:12:49 +0300

    added increment likes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -38,6 +38,16 @@ class BirdsController < ApplicationController
     end
   end
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
 
 
 
