d8f7e812f76badbd9f2b2bc533c6fb06802c9d1b
Eric Mugo <mugoericpg20@gmail.com>
2023-06-23 16:12:25 +0300

    update method

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -25,7 +25,21 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
+
+  def update
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+
+
+
 
 end
